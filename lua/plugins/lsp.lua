35e62f8b6448b3523091dfc3bfa5b2389c7b04ac
AdryDev <adrydevmateo@gmail.com>
2024-10-13 21:57:41 -0400

    Set up completion

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,10 +3,12 @@ return {
 	"neovim/nvim-lspconfig",
 	config = function()
 		local lsp = require("lspconfig")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 		lsp.gopls.setup({
 			cmd = { "gopls" },
 			filetypes = { "go", "gomod", "gowork", "gotmpl" },
+			capabilities = capabilities
 		})
 	end
 	}
