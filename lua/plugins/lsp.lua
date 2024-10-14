6967855b51a6527cbc47a33e636b70629da15134
AdryDev <adrydevmateo@gmail.com>
2024-10-13 23:14:22 -0400

    Added tsserver now named ts_ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,18 +5,18 @@ return {
 			local lsp = require("lspconfig")
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+			lsp.ts_ls.setup({
+				cmd = { "typescript-language-server", "--stdio" },
+				filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+				root_dir = lsp.util.root_pattern("tsconfig.json", "jsconfig.json", ".git"),
+			})
+
 			lsp.gopls.setup({
 				cmd = { "gopls" },
 				filetypes = { "go", "gomod", "gowork", "gotmpl" },
 				capabilities = capabilities,
+				root_dir = lsp.util.root_pattern("go.mod", ".git"),
 			})
-
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true })
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true, silent = true })
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { noremap = true, silent = true })
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { noremap = true, silent = true })
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { noremap = true, silent = true })
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { noremap = true, silent = true })
 		end,
 	},
 }
