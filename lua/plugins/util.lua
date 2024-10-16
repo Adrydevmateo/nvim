a4948c5c5377194f7c99a9c095fd36eafffbc10e
AdryDev <adrydevmateo@gmail.com>
2024-10-16 19:30:03 -0400

    reset

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -20,25 +20,4 @@ return {
 			})
 		end,
 	},
-	-- Add formatter.nvim
-	{
-		"stevearc/conform.nvim",
-		config = function()
-			require("conform").setup({
-				formatters_by_ft = {
-					lua = { "stylua" },
-					javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
-					typescript = { "biome", "prettierd", "prettier", stop_after_first = true },
-					typescriptreact = { "biome", "prettierd", "prettier", stop_after_first = true },
-				},
-			})
-
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				pattern = "*",
-				callback = function(args)
-					require("conform").format({ bufnr = args.buf })
-				end,
-			})
-		end,
-	},
 }
