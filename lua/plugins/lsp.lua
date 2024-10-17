236be924856ac64ad77d0729deb44cb6cd982668
AdryDev <adrydevmateo@gmail.com>
2024-10-16 20:27:02 -0400

    Added ts_ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,6 +4,15 @@ return {
 		event = { "BufReadPre", "BufNewFile" },
 		config = function()
 			local lsp = require("lspconfig")
+			local util = require("lspconfig.util")
+
+			lsp.ts_ls.setup({
+				cmd = { "typescript-language-server", "--stdio" },
+				filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+				init_options = { hostInfo = "neovim" },
+				root_dir = util.root_pattern("tsconfig.json", "jsconfig.json", "package.json", ".git"),
+				single_file_support = true
+			})
 		end
 	}
 }
