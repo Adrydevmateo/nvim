95057d18de074187cc51dd1e07eee59307889949
AdryDev <adrydevmateo@gmail.com>
2024-10-17 20:41:05 -0400

    Change indentation

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -6,3 +6,10 @@ vim.cmd('colorscheme kanagawa')
 -- [ Numbers ]
 vim.cmd('set relativenumber')
 vim.cmd('set numberwidth=3')
+
+-- [ Indentation ]
+vim.cmd('set tabstop=4')    -- Number of spaces a <Tab> counts for
+vim.cmd('set shiftwidth=4') -- Number of spaces to use for autoindent
+vim.cmd('set expandtab')    -- Convert tabs to spaces
+vim.cmd('set autoindent')   -- Copy indent from current line when starting a new line
+vim.cmd('set smartindent')  -- Makes indenting smart (for languages like C)
