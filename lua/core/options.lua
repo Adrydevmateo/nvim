52ce7c0a846e3224c729850a6319e4123f6765c7
AdryDev <adrydevmateo@gmail.com>
2024-10-20 11:40:12 -0400

    Added keymap to save on insert mode and disabled line numbers

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -4,8 +4,8 @@
 vim.cmd('colorscheme kanagawa')
 
 -- [ Numbers ]
-vim.cmd('set relativenumber')
-vim.cmd('set numberwidth=3')
+-- vim.cmd('set relativenumber')
+-- vim.cmd('set numberwidth=3')
 
 -- [ Indentation ]
 vim.cmd('set tabstop=4')    -- Number of spaces a <Tab> counts for
@@ -13,3 +13,6 @@ vim.cmd('set shiftwidth=4') -- Number of spaces to use for autoindent
 vim.cmd('set expandtab')    -- Convert tabs to spaces
 vim.cmd('set autoindent')   -- Copy indent from current line when starting a new line
 vim.cmd('set smartindent')  -- Makes indenting smart (for languages like C)
+
+-- Responsiveness
+vim.cmd('set timeoutlen=300')
