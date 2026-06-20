ffdde7194b6c178294d732d95d4970e88b72aa23
Adrydevmateo <adrydevmateo@gmail.com>
2026-06-19 22:00:52 -0400

    to publish

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,26 +1,28 @@
 local opt = vim.opt
 
-opt.number = true		      -- Show line numbers
+opt.number = true -- Show line numbers
 opt.relativenumber = true -- Show relative line numbers
-opt.scrolloff = 10		    -- Keep 8 lines above/below cursor
-opt.sidescrolloff = 2		  -- Keep 8 columns left/right of cursor
-opt.showbreak = "↪ "		  -- Show break indicator
+opt.scrolloff = 2 -- Keep 8 lines above/below cursor
+opt.sidescrolloff = 2 -- Keep 8 columns left/right of cursor
+opt.showbreak = "↪ " -- Show break indicator
+opt.spell = true -- Enable spell checking
+opt.spelllang = { "en_us" } -- Set spell checker language
 
 -- indentation
-opt.expandtab = true	  -- Use spaces instead of tabs
-opt.shiftwidth = 2	    -- Number of spaces for auto-indent
-opt.tabstop = 2		      -- Number of spaces that a tab counts for
-opt.softtabstop = 2	    -- Number of spaces for tab in insert mode
-opt.autoindent = true	  -- Copy indent from current line
-opt.smartindent = true  -- Smart auto-indent
+opt.expandtab = true -- Use spaces instead of tabs
+opt.shiftwidth = 2 -- Number of spaces for auto-indent
+opt.tabstop = 2 -- Number of spaces that a tab counts for
+opt.softtabstop = 2 -- Number of spaces for tab in insert mode
+opt.autoindent = true -- Copy indent from current line
+opt.smartindent = true -- Smart auto-indent
 
 -- search settings
 opt.ignorecase = true -- Ignore case in search
-opt.smartcase = true  -- Don't ignore case if search contains uppercase
+opt.smartcase = true -- Don't ignore case if search contains uppercase
 
 -- file handling
 opt.autoread = true -- Auto-reload files changed outside vim
 
 -- ui behavior
-opt.cursorline = true     -- Highlight current line
-opt.termguicolors = true  -- Enable true color
+opt.cursorline = true -- Highlight current line
+opt.termguicolors = true -- Enable true color
